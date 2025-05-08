8dbe49bddf9eec72ea606f4b3cbd36c315e67d5c
Christof Konstantinopoulos <ckonstantinopoulos@pulmokard.de>
2025-05-08 16:01:37 +0200

    Programm Dokumentation hinzugefügt

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -15,24 +15,26 @@ namespace ConsoleApp4
             // da in der Aufgabe steht, dass die Zahlen bis 100 zurückgegben werden sollen.
             for (int i = 1; i <= 100; i++)
             {
+                // Ausgabebuffer leeren
                 sb.Clear();
-                // wenn die Zahl durch 6 teilbar ist, dann gebe " " vor Fizz aus
+                // wenn die Zahl durch 6 teilbar ist,  dann " " vor Buzz einfügen
                 if (i % 6 == 0) sb.Append(" ");
-                // wenn die Zahl durch 3 teilbar ist, dann gebe "Fizz" aus
+                // wenn die Zahl durch 3 teilbar ist, dann "Fizz" vormerken
                 if (i % 3 == 0) sb.Append("Fizz");
-                // wenn die Zahl durch 10 teilbar ist, dann gebe " " vor Buzz aus
+                // wenn die Zahl durch 10 teilbar ist, dann " " vor Buzz einfügen
                 if (i % 10 == 0) sb.Append(" ");
-                // wenn die Zahl durch 5 teilbar ist, dann gebe "Buzz" aus
+                // wenn die Zahl durch 5 teilbar ist, dann "Buzz" vormerken
                 if (i % 5 == 0) sb.Append("Buzz");
-                // wenn die Zahl durch 7 teilbar ist, dann gebe "Jazz" aus
+                // wenn die Zahl durch 7 teilbar ist, dann "Jazz" vormerken
                 if (i % 7 == 0) sb.Append("Jazz");
 
-                if (sb.Length == 0)
-                {
-                    sb.Append($"{i:D}");
-                }
+                // wenn kein Text vorgemerkt wurde, dann die Zahl selbst in den StringBuilder einfügen
+                if (sb.Length == 0) sb.Append($"{i:D}");
 
+                // Zeilen Umbruch hinzufügen
                 sb.AppendLine();
+                
+                // Text ausgeben
                 Console.Write(sb.ToString());
             }
         }
