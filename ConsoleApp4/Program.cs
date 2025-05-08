95afae0719c3cdabd0189f08b408e9466e3f1980
mjbeh45 <mohammadjubeh3@gmail.com>
2025-05-08 16:00:35 +0200

    Erweiterung von "FizzBuzzJazz" , "FizzBuzz", "BuzzJazz", "FizzJazz" und "Jazz"

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,26 +15,51 @@ namespace ConsoleApp4
             // da in der Aufgabe steht, dass die Zahklen bis 100 zurückgegben werden sollen.
             for (int i = 1; i <= 100; i++)
             {
-              // wenn die Zahl durch 3 und 5 teilbar ist, dann gebe "FizzBuzz" aus
-                 if (i % 3 == 0 && i % 5 == 0)
+                //wenn die Zahl durch 7,5 und 3 teilbar ist, dann gebe "FizzBuzz" aus
+                if (i % 7 == 0 && i % 5 == 0 && i % 3 == 0)
+                {
+                    Console.WriteLine("FizzBuzzJazz");
+                }
+                // wenn die Zahl durch 3 und 5 teilbar ist, dann gebe "FizzBuzz" aus
+                else if (i % 3 == 0 && i % 5 == 0)
                 {
                     Console.WriteLine("FizzBuzz");
                 }
 
+                // wenn die Zahl durch 7 und 5 teilbar ist, dann gebe "FizzBuzz" aus
+                else if (i % 7 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("BuzzJazz");
+
+                }
+                // wenn die Zahl durch 7 und 3 teilbar ist, dann gebe "FizzBuzz" aus
+                else if (i % 7 == 0 && i % 3 == 0)
+                {
+                    Console.WriteLine("FizzJazz");
+
+                }
+
+                // wenn die Zahl durch 7teilbar ist, dann gebe "Fizz" aus
+
+                else if (i % 7 == 0)
+                {
+                    Console.WriteLine("Jazz");
+                }
                 // wenn die Zahl durch 3 teilbar ist, dann gebe "Fizz" aus
 
-                else if (i % 3==0)
+                else if (i % 3 == 0)
                 {
                     Console.WriteLine("Fizz");
                 }
                 // wenn die Zahl durch 5 teilbar ist, dann gebe "Buzz" aus
-                
-                else if (i % 5==0)
+
+                else if (i % 5 == 0)
                 {
                     Console.WriteLine("Buzz");
                 }
 
-                else {
+                else
+                {
                     Console.WriteLine(i);
                 }
                      
