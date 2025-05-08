4f3ba7648e7955016de99b472c5dd48df9032be7
Christof Konstantinopoulos <ckonstantinopoulos@pulmokard.de>
2025-05-08 15:56:04 +0200

    Refactoring Aufgabe 4 - Keine mehrfachen Bedingungen im if Statement nutzen!

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -11,38 +11,30 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
-            // da in der Aufgabe steht, dass die Zahklen bis 100 zurückgegben werden sollen.
+            var sb = new StringBuilder();
+            // da in der Aufgabe steht, dass die Zahlen bis 100 zurückgegben werden sollen.
             for (int i = 1; i <= 100; i++)
             {
-              // wenn die Zahl durch 3 und 5 teilbar ist, dann gebe "FizzBuzz" aus
-                 if (i % 3 == 0 && i % 5 == 0)
-                {
-                    Console.WriteLine("FizzBuzz");
-                }
-
+                sb.Clear();
+                // wenn die Zahl durch 6 teilbar ist, dann gebe " " vor Fizz aus
+                if (i % 6 == 0) sb.Append(" ");
                 // wenn die Zahl durch 3 teilbar ist, dann gebe "Fizz" aus
-
-                else if (i % 3==0)
-                {
-                    Console.WriteLine("Fizz");
-                }
+                if (i % 3 == 0) sb.Append("Fizz");
+                // wenn die Zahl durch 10 teilbar ist, dann gebe " " vor Buzz aus
+                if (i % 10 == 0) sb.Append(" ");
                 // wenn die Zahl durch 5 teilbar ist, dann gebe "Buzz" aus
-                
-                else if (i % 5==0)
+                if (i % 5 == 0) sb.Append("Buzz");
+                // wenn die Zahl durch 7 teilbar ist, dann gebe "Jazz" aus
+                if (i % 7 == 0) sb.Append("Jazz");
+
+                if (sb.Length == 0)
                 {
-                    Console.WriteLine("Buzz");
+                    sb.Append($"{i:D}");
                 }
 
-                else {
-                    Console.WriteLine(i);
-                }
-                     
-
-                
-
+                sb.AppendLine();
+                Console.Write(sb.ToString());
             }
-
-          
         }
     }
 }
